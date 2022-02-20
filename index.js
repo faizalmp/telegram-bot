464fc2ddfaf7e51525d75acb6359dfa098e379b6
faizalpriyowibowo <faizalpriyowibowo@gmail.com>
2022-02-20 14:07:18 +0700

    try node telegram bot api

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const { Telegraf } = require("telegraf")
-const bot = new Telegraf(process.env.BOT_TOKEN)
+require('dotenv').config()
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true })
 
-bot.start((ctx) => ctx.reply('Welcome to signal bot'))
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, 'Received your message');
+});
