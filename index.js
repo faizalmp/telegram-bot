63cb74fd9fe29b42941687830b5c56b14fc61e5b
faizalpriyowibowo <faizalpriyowibowo@gmail.com>
2022-02-27 09:18:59 +0700

    set webhook

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 require('dotenv').config()
 const express = require('express')
-const TelegramBot = require('node-telegram-bot-api')
 
 const app = express()
 const port = process.env.PORT || 3000
-const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true })
 
 app.use(cors())
 app.options('*', cors())
@@ -16,12 +14,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
-});
